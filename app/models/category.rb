0006954a066912631e8f0583cf4b9417983100e7
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-09-24 20:08:32 +0530

    Fix linters error

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,6 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :category_groups
   belongs_to :user
   attribute :amount, :integer
-  
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
 end
